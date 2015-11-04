8818f83a1c16ea0ef8a0ded417ccaee40c366747
Lissa Walzer <Lissa@Eric-30.local>
2015-11-04 13:16:01 -0800

    Added a gradient

--- script.js
+++ script.js
@@ -8,16 +8,18 @@ var _body = document.getElementsByTagName('body')[0];
 
 function Color(arr) {
 	var count = 0;
+	var color = 0;
 	for (var i = 0; i < arr.length; i++) {
+		color += 5;
 		count++;
 		arr[i].style.float = "left";
 		arr[i].style.width = "11.1%";
 		arr[i].style.height = "11.1%"
 		arr[i].style.paddingBottom = "11.1%"
 		if (count % 2 === 0) {
-			arr[i].style.backgroundColor = "red";			
+			arr[i].style.backgroundColor = "rgb(" + color + ", 250, 127)";			
 		} else {
-			arr[i].style.backgroundColor = "black";
+			arr[i].style.backgroundColor = "rgb(" + color + ",100, 170)";
 		}
 	}
 }
