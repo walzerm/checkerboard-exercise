ff81df5b7038d7495a447f60d3d3bf7360b48e8c
Lissa Walzer <Lissa@Eric-30.local>
2015-11-04 12:05:55 -0800

    Added the plain checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// Your JS goes here
+var arr = [];
+var _body = document.getElementsByTagName('body')[0];
+	for (var i = 0; i < 9 * 7; i++) {	
+		var square = document.createElement("div");
+		arr.push(square);
+		_body.appendChild(square);
+	}
+
+function Color(arr) {
+	var count = 0;
+	for (var i = 0; i < arr.length; i++) {
+		count++;
+		arr[i].style.float = "left";
+		arr[i].style.width = "11.1%";
+		arr[i].style.height = "11.1%"
+		arr[i].style.paddingBottom = "11.1%"
+		if (count % 2 === 0) {
+			arr[i].style.backgroundColor = "red";			
+		} else {
+			arr[i].style.backgroundColor = "black";
+		}
+	}
+}
+Color(arr);
